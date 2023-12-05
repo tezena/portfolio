9fa374862ebaeaaa7c761abd643b3863e4f4abac
tezena <tewodrosnibretadal2012@gmail.com>
2023-12-05 05:44:28 -0800

    about text

--- src/app/Home/components/AboutSection.js
+++ src/app/Home/components/AboutSection.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const AboutSection = () => {
   return (
-    <div className="h-[50%] bg-amber-400">
-      <h1>
+    <div className="h-[50%] bg-amber-400 flex items-center lg:p-32 lg:text-3xl text-center font-montserrat font-semibold ">
+      <h1 className="leading-relaxed text-slate-100">
         My journey has encompassed dynamic contributions to web and mobile
         application development projects, equipping me with proficient skills in
         MERN stack and Flutter development.
